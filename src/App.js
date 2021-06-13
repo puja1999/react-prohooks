f8ca7988d02968492c745d81ab4470e93537d105
Puja <pujasah2501@gmai.com>
2021-06-13 15:37:12 +0530

    completed

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Search } from './Search';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { LoginCallback, SecureRoute, Security } from '@okta/okta-react';
+import { Home } from './Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <header>
+          <div>ProHooks</div>
+          <ul className="menu"><li><Link to="/">Home</Link></li><li><Link to="/search">Search</Link></li></ul>
+        </header>
+        <Security issuer='https://dev-28362205.okta.com/oauth2/default'
+                  clientId='0oa104pr7wOvr2cbr5d7'
+                  redirectUri='http://localhost:3000/callback'
+                  pkce={true}>
+          <Route path='/' exact={true} component={Home}/>
+        <SecureRoute path='/search' exact={true} component={Search}/>
+          <Route path='/callback' component={LoginCallback}/>
+        </Security>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
